9c8c20fb82f0117e6e4a6952cebed1d22bb018ea
K-MHOON <azmria117@gmail.com>
2023-12-29 23:01:14 +0900

    feat : react-router-dom 추가 및 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { RouterProvider } from "react-router-dom";
+import root from "./router/root";
 
 const App = () => {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return <RouterProvider router={root} />;
 };
 
 export default App;
