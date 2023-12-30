b69cc9cf9f6a1fc874c5519d3e8a045bcf683976
K-MHOON <azmria117@gmail.com>
2023-12-30 10:44:52 +0900

    feat : 기본 레이아웃 컴포넌트 추가 및 About 컴포넌트 적용

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,11 @@
 import React from "react";
+import BasicLayout from "../layouts/BasicLayout";
 
 const AboutPage = () => {
     return (
-        <div className="text-3xl">
-            <div>About Page</div>
-        </div>
+        <BasicLayout>
+            <div className="text-3xl">About Page</div>
+        </BasicLayout>
     );
 };
 
