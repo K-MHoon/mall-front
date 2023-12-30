50c263488295514fdc8be17b65ee020209942d72
K-MHOON <azmria117@gmail.com>
2023-12-30 10:53:27 +0900

    feat : todo 메뉴 레이아웃 추가, router 적용

--- src/router/root.js
+++ src/router/root.js
@@ -4,6 +4,7 @@ import { createBrowserRouter } from "react-router-dom";
 const Loading = <div>Loading....</div>;
 const Main = lazy(() => import("../pages/MainPage"));
 const About = lazy(() => import("../pages/AboutPage"));
+const TodoIndex = lazy(() => import("../pages/todo/IndexPage"));
 
 const root = createBrowserRouter([
     {
@@ -19,6 +20,14 @@ const root = createBrowserRouter([
         element: (
             <Suspense fallback={Loading}>
                 <About />
+            </Suspense>
+        ),
+    },
+    {
+        path: "todo",
+        element: (
+            <Suspense fallback={Loading}>
+                <TodoIndex />
             </Suspense>
         ),
     },
