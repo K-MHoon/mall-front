dd7cf508fcc6d794857a8b8a1fbe48b4a5a5243c
K-MHOON <azmria117@gmail.com>
2024-02-10 19:59:15 +0900

    feat : 카카오 로그인시 최초 소셜로그인인경우 회원정보 수정을 이동

---
+++ src/pages/member/KakaoRedirectPage.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { getAccessToken, getMemberWithAccessToken } from "../../api/kakaoApi";
+import { useDispatch } from "react-redux";
+import { login } from "../../slices/loginSlice";
+import useCustomLogin from "../../hooks/useCustomLogin";
+
+const KakaoRedirectPage = () => {
+    const [searchParams] = useSearchParams();
+    const dispatch = useDispatch();
+    const { moveToPath } = useCustomLogin();
+    const authCode = searchParams.get("code");
+
+    useEffect(() => {
+        getAccessToken(authCode).then((accessToken) => {
+            console.log(accessToken);
+
+            getMemberWithAccessToken(accessToken).then((memberInfo) => {
+                console.log("----------------");
+                console.log(memberInfo);
+
+                dispatch(login(memberInfo));
+
+                if (memberInfo && !memberInfo.social) {
+                    moveToPath("/");
+                } else {
+                    moveToPath("/member/modify");
+                }
+            });
+        });
+    }, [authCode]);
+
+    return (
+        <div>
+            <div>Kakako Login Redirect</div>
+            <div>{authCode}</div>
+        </div>
+    );
+};
+
+export default KakaoRedirectPage;
