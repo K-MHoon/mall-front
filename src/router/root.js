0e8e8167e9e7a7d86eb7563e3267cf0719d1441a
K-MHOON <azmria117@gmail.com>
2024-02-11 22:05:07 +0900

    feat : Product 라우터, member 라우터 연결 추가, AboutPage 페이지 추가

--- src/router/root.js
+++ src/router/root.js
@@ -1,11 +1,14 @@
 import { Suspense, lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import todoRouter from "./todoRouter";
+import productRouter from "./productRouter";
+import memberRouter from "./memberRouter";
 
 const Loading = <div>Loading....</div>;
 const Main = lazy(() => import("../pages/MainPage"));
 const About = lazy(() => import("../pages/AboutPage"));
 const TodoIndex = lazy(() => import("../pages/todo/IndexPage"));
+const ProductsIndex = lazy(() => import("../pages/products/IndexPage"));
 
 const root = createBrowserRouter([
     {
@@ -32,6 +35,19 @@ const root = createBrowserRouter([
             </Suspense>
         ),
         children: todoRouter(),
+    },
+    {
+        path: "products",
+        element: (
+            <Suspense fallback={Loading}>
+                <ProductsIndex />
+            </Suspense>
+        ),
+        children: productRouter(),
+    },
+    {
+        path: "member",
+        children: memberRouter(),
     },
 ]);
 
