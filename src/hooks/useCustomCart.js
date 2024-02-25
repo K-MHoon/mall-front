c4d01b95b7d5be4e090392b71dbb9d2025a038db
K-MHOON <azmria117@gmail.com>
2024-02-25 19:42:33 +0900

    fix : 장바구니 공통 훅 리코일, 리액트 쿼리 사용하도록 수정

--- src/hooks/useCustomCart.js
+++ src/hooks/useCustomCart.js
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCartItemsAsync, postChangeCartAsync } from "../slices/cartSlice";
+import { useRecoilState } from "recoil";
+import { cartState } from "../atoms/cartState";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getCartItems, postChangeCart } from "../api/cartApi";
 
 const useCustomCart = () => {
-    const cartItems = useSelector((state) => state.cartSlice);
-    const dispatch = useDispatch();
+    // const cartItems = useSelector((state) => state.cartSlice);
+    const [cartItems, setCartItmes] = useRecoilState(cartState);
 
-    const refreshCart = () => {
-        dispatch(getCartItemsAsync());
-    };
+    const queryClient = useQueryClient();
+
+    const changeMutation = useMutation({
+        mutationFn: (param) => postChangeCart(param),
+        onSuccess: (result) => setCartItmes(result),
+    });
+
+    const query = useQuery({
+        queryKey: ["cart"],
+        queryFn: () => getCartItems(),
+        staleTime: 1000 * 60 * 60,
+    });
+
+    // const dispatch = useDispatch();
+
+    // const refreshCart = () => {
+    //     dispatch(getCartItemsAsync());
+    // };
+
+    useEffect(() => {
+        if (query.isSuccess || changeMutation.isSuccess) {
+            queryClient.invalidateQueries("cart");
+            setCartItmes(query.data);
+        }
+    }, [query.isSuccess, query.data]);
 
     const changeCart = (param) => {
-        dispatch(postChangeCartAsync(param));
+        // dispatch(postChangeCartAsync(param));
+        changeMutation.mutate(param);
     };
 
-    return { cartItems, refreshCart, changeCart };
+    return { cartItems, changeCart };
 };
 
 export default useCustomCart;
